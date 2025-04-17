df48526851200d5ad29d8fc3e59e11e323c1c87f
nicolasromeroo <145489907+nicolasromeroo@users.noreply.github.com>
2025-04-17 19:01:50 -0300

    Actualizar server.js

--- server-inventario/src/server.js
+++ server-inventario/src/server.js
@@ -12,7 +12,7 @@ const app = express()
 app.use(express.json())
 app.use(cors({
     origin: ["http://localhost:5173", 
-    “https://inventario-git-master-nicolas-projects-a0f909f1.vercel.app"]
+    "https://inventario-git-master-nicolas-projects-a0f909f1.vercel.app"]
 }))
 
 app.use("/", productsRouter)
